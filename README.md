b10cc61709a4c2f22d34157928702da9bf4581ca
mightyfrog <mightyfrog.gc@gmail.com>
2017-07-30 03:20:24 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Simple-GraphQL-Server
-A simple GraphQL server sample for shibuya.apk #17
+# Simple-GraphQL-API-Server
+A simple GraphQL API server sample for shibuya.apk #17
+
+## Download dependencies:
+```text
+npm install --save
+```
+
+## Start GraphQL API server:
+```text
+node server.js
+```
+The server starts running on localhost: http://localhost:4000/graphql
+
+## Queries:
+- allMovies: [Movie!]
+```javascript
+{
+  allMovies {
+    id
+    title
+    year
+    rating
+  }
+}
+```
+- movie(id: ID!): Movie
+```javascript
+movie(id:"8e989b68-34a6-40ca-a33b-e0b7f7420cdc") {
+  id
+  title
+}
+```
+## Mutation:
+- addMovie(movie: MovieInput!): Movie!
+```javascript
+mutation addMovie($movie: MovieInput!) {
+  addMovie(movie: $movie) {
+    id
+  }
+}
+
+{
+  "movie": {
+    "title": "The Shining",
+    "year": 1980,
+    "rating": 8.4
+  }
+}
+```
+See GraphiQL Documentation Explorer for more details.
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var uuidV4 = require('uuid/v4');
+
+var movies = {}
+
+var schema = buildSchema(`
+  type Movie {
+      id: ID!
+      title: String!
+      year: Int!
+      rating: Float!
+  }
+
+  input MovieInput {
+    title: String!
+    year: Int!
+    rating: Float!
+  }
+
+  type Query {
+    allMovies: [Movie!]
+    movie(id: ID!): Movie
+  }
+
+  type Mutation {
+    addMovie(movie: MovieInput!): Movie!
+  }
+`)
+
+var root = {
+    allMovies: function() {
+        return Object.keys(movies).map(function(k) { return movies[k] })
+    },
+
+    movie: function({ id }) {
+        return movies[id];
+    },
+
+    addMovie: function({ movie }) {
+        var item = {
+            "id": uuidV4(),
+            "title": movie.title,
+            "year": movie.year,
+            "rating": movie.rating,
+            "___type": "Movie"
+        };
+
+        movies[item.id] = item;
+
+        return item;
+    }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true }));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
